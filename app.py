f034d6b83916e4e83317268098c5769ead3f7d8e
Rengin <4renatit@gmail.com>
2020-12-01 21:39:40 -0800

    Module Update10.5.3

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ def scrape():
    mars.update({}, mars_data, upsert=True)
    return "Scraping Successful!"
 
-.update(query_parameter, data, options)
+if __name__ == "__main__":
+   app.run()
 
-selin
+#selin
+
